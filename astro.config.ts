5cbf88303f5b1c5eb7fbac1df9a8148443ab9584
NihilAkatsuki <Nihil1254417980@outlook.com>
2026-05-08 02:30:21 -0700

    add github actions deploy

--- astro.config.ts
+++ astro.config.ts
@@ -11,9 +11,9 @@ import {
 import { transformerFileName } from "./src/utils/transformers/fileName";
 import { SITE } from "./src/config";
 
-// https://astro.build/config
 export default defineConfig({
-  site: SITE.website,
+	site: 'https://NihilAkatsuki.github.io',
+  base: '/Math-blog',
   integrations: [
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
